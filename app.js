023a54f15f1def90393eaf5ccc9f591b198e2c8c
shrajan9696 <jainshrajan482@gmail.com>
2022-04-02 01:02:56 +0530

    level 3 security with hashing

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 mongoose.connect("mongodb://localhost:27017/userDB");
 
@@ -27,7 +27,6 @@ const userSchema =new mongoose.Schema({
 });
 
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET,encryptedFields: ['password']});
 
 const User = new mongoose.model("User", userSchema);
 //TODO
@@ -46,7 +45,7 @@ app.get("/login",function(req,res){
 app.post("/register",function(req,res){
   const newUser = new User({
     email:req.body.username,
-    password:req.body.password
+    password:md5(req.body.password)
   });
 
   newUser.save(function(err){
@@ -61,7 +60,7 @@ app.post("/register",function(req,res){
 
 app.post("/login",function(req,res){
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({email:username},function(err,foundUser){
     if(err){
@@ -71,6 +70,9 @@ app.post("/login",function(req,res){
       if(foundUser){
         if(foundUser.password===password){
           res.render("secrets");
+        }
+        else{
+          res.send("wrong password");
         }
       }
     }
